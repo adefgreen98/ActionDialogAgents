6e56b495e50503fc9489f0db33fd005c8e716829
adefgreen98 <adefgreen98@gmail.com>
2022-05-16 11:49:12 +0200

    Added support for additional embedding layers; General refactoring

--- multimodal/vect_models.py
+++ multimodal/vect_models.py
@@ -5,6 +5,20 @@ import torch.nn as nn
 from abc import ABC, abstractmethod
 from typing import Tuple
 
+
+__allowed_conditioning_methods__ = ['concat', 'embedding']
+__allowed_activations__ = {
+    'none': nn.Identity,
+    'relu': nn.ReLU,
+    'sigmoid': nn.Sigmoid,
+    'tanh': nn.Tanh
+}
+
+__shared_embedding_size__ = 256
+__action_embedding_size__ = 768
+
+
+
 class AbstractVectorTransform(ABC, nn.Module):
     """Base class for all models of Vector Transformation (VecT)."""
 
@@ -18,16 +32,16 @@ class AbstractVectorTransform(ABC, nn.Module):
         self.device = device
 
     @abstractmethod
-    def forward(self, v, action):
+    def forward(self, before, action, after, negatives):
         pass
 
     def process_batch(self, batch, loss_fn=None):
-        before, actions, after = batch
-        before = before.to(self.device)
-        actions = actions.to(self.device).long()
-        after = after.to(self.device)
+        before = batch['before'].to(self.device)
+        actions = batch['action'].to(self.device).long()
+        after = batch['positive'].to(self.device)
+        negatives = batch['negatives'].to(self.device)
 
-        predictions = self(before, actions)
+        before, predictions, after, negatives = self(before, actions, after, negatives)
 
         if self.training and loss_fn is not None:
             return loss_fn(predictions, after)
@@ -39,8 +53,42 @@ class AbstractVectorTransform(ABC, nn.Module):
     def to(self, *args, **kwargs):
         self.device = args[0]
         res = super(AbstractVectorTransform, self).to(*args, **kwargs)
-        # res.device = args[0]
         return res
+
+    def setup_embedding_layers(self, hidden_sizes):
+        layer_sizes = [self.vec_size, self.vec_size]
+
+        if self.conditioning_method == 'concat':
+            layer_sizes[0] += self.nr_actions
+
+            self.__eye_helper = torch.eye(self.nr_actions, device=self.device)
+
+            def embed_action(action_batch: torch.LongTensor) -> torch.Tensor:
+                return torch.stack([self.__eye_helper[i].detach().clone() for i in action_batch], dim=0).to(self.device)
+
+            self.embed_visual = nn.Identity()
+            self.embed_action = embed_action
+
+        elif self.conditioning_method == 'embedding':
+            layer_sizes[0] = 2 * __shared_embedding_size__
+            layer_sizes[-1] = __shared_embedding_size__
+            self.embed_visual = nn.Linear(self.vec_size, __shared_embedding_size__)
+            self.embed_action = nn.Sequential(
+                nn.Embedding(self.nr_actions, __action_embedding_size__),
+                nn.Linear(__action_embedding_size__, __shared_embedding_size__)
+            )
+
+        if hidden_sizes is None:
+            h = int(layer_sizes[0] * 0.75)
+            hidden_sizes = [h]
+        elif hidden_sizes == 'large':
+            h = int(layer_sizes[0] * 0.75)
+            hidden_sizes = [h, h // 2, h]
+        self.hidden_sizes = hidden_sizes
+
+        layer_sizes = [layer_sizes[0]] + hidden_sizes + [layer_sizes[-1]]
+
+        return layer_sizes
 
 
 __supported_regressions__ = {None, 'torch'}
@@ -64,10 +112,10 @@ class LinearVectorTransform(AbstractVectorTransform):
         self.weights = nn.Parameter(torch.stack([nn.Linear(self.vec_size, self.vec_size, bias=False).weight.clone().detach() for i in range(self.nr_actions)], dim=0))
 
 
-    def forward(self, v, action: str):
+    def forward(self, before, action, after, negatives):
         # Normalize vector?
-        pred = v.unsqueeze(1).bmm(self.weights.index_select(0, action))
-        return pred.squeeze(1)
+        pred = before.unsqueeze(1).bmm(self.weights.index_select(0, action))
+        return before, pred.squeeze(1), after, negatives
 
     def regression_init(self, data: Tuple, regression_type=None):
         # data is a tuple of dicts ({'action' --> samples matrix A}, {'action' --> target vector B})
@@ -102,30 +150,21 @@ class LinearVectorTransform(AbstractVectorTransform):
 
 
 class LinearConcatVecT(AbstractVectorTransform):
-    def __init__(self, actions: set, vec_size: int, **kwargs):
+    def __init__(self, actions: set, vec_size: int, conditioning_method='concat', **kwargs):
         super().__init__(actions, vec_size, **kwargs)
 
-        self.__eye_helper = torch.eye(self.nr_actions, device=self.device)
+        self.conditioning_method = conditioning_method
 
-        def embed_action(action_batch: torch.LongTensor) -> torch.Tensor:
-            return torch.stack([self.__eye_helper[i].detach().clone() for i in action_batch], dim=0).to(self.device)
+        layer_sizes = self.setup_embedding_layers([])
 
-        self.embed_action = embed_action
-        self.net = nn.Linear(self.nr_actions + self.vec_size, self.vec_size, bias=False)
+        self.net = nn.Linear(layer_sizes[0], layer_sizes[-1])
 
-    def forward(self, v, action):
-        embedded = self.embed_action(action)
-        return self.net(torch.cat((v, embedded), dim=-1))
-
-
-
-__allowed_conditioning_methods__ = ['concat']
-__allowed_activations__ = {
-    'none': nn.Identity,
-    'relu': nn.ReLU,
-    'sigmoid': nn.Sigmoid,
-    'tanh': nn.Tanh
-}
+    def forward(self, before, action, after, negatives):
+        before = self.embed_visual(before)
+        pred = self.net(torch.cat([before, self.embed_action(action)], dim=-1))
+        after = self.embed_visual(after)
+        negatives = self.embed_visual(negatives)
+        return before, pred, after, negatives
 
 
 class ConditionalFCNVecT(AbstractVectorTransform):
@@ -149,26 +188,7 @@ class ConditionalFCNVecT(AbstractVectorTransform):
         assert conditioning_method in __allowed_conditioning_methods__
         self.conditioning_method = conditioning_method
 
-        if hidden_sizes is None:
-            h = int(self.vec_size * 0.75)
-            hidden_sizes = [h]
-        elif hidden_sizes == 'large':
-            h = int(self.vec_size * 0.75)
-            hidden_sizes = [h, h // 2, h]
-        self.hidden_sizes = hidden_sizes
-
-        layer_sizes = [self.vec_size] + hidden_sizes + [self.vec_size]
-
-        if self.conditioning_method == 'concat':
-            layer_sizes[0] += self.nr_actions
-            # self.embed_actions = nn.Embedding(self.nr_actions, self.nr_actions)  # TODO understand if this works
-
-            self.__eye_helper = torch.eye(self.nr_actions, device=self.device)
-
-            def embed_action(action_batch: torch.LongTensor) -> torch.Tensor:
-                return torch.stack([self.__eye_helper[i].detach().clone() for i in action_batch], dim=0).to(self.device)
-
-            self.embed_action = embed_action
+        layer_sizes = self.setup_embedding_layers(hidden_sizes)
 
         # TODO: check ordering of BatchNorm and activation
         self.net = nn.Sequential(*[
@@ -183,56 +203,10 @@ class ConditionalFCNVecT(AbstractVectorTransform):
                 for i in range(len(layer_sizes) - 1)]  # because building with (i, i+1)
             )
 
-    def forward(self, v, action):
-        return self.net(torch.cat([v, self.embed_action(action)], dim=-1))
+    def forward(self, before, action, after, negatives):
+        before = self.embed_visual(before)
+        pred = self.net(torch.cat([before, self.embed_action(action)], dim=-1))
+        after = self.embed_visual(after)
+        negatives = self.embed_visual(negatives)
+        return before, pred, after, negatives
 
-
-
-class PolyFCNVecT(AbstractVectorTransform):
-    """Module implementing a vector transformation with a unique neural network for each action."""
-
-    def __init__(self, actions: set, vec_size: int,
-                 hidden_sizes=None,
-                 dropout_p: float = 0.0,
-                 activation='none',
-                 use_bn: bool = False,
-                 **kwargs):
-        super(PolyFCNVecT, self).__init__(actions, vec_size)
-
-        self.dropout_p = dropout_p
-        self.use_bn = use_bn
-
-        assert activation in __allowed_activations__
-        self.activation = activation
-
-        if hidden_sizes is None:
-            h = int(self.nr_actions * 0.75)
-            hidden_sizes = [h]
-        elif hidden_sizes == 'large':
-            h = int(self.nr_actions * 0.75)
-            hidden_sizes = [h, h // 2, h]
-        self.hidden_sizes = hidden_sizes
-
-        layer_sizes = [self.vec_size] + hidden_sizes + [self.vec_size]
-
-        self.transforms = {
-            k: nn.Sequential(*[
-                nn.Sequential(
-                    OrderedDict([
-                        ('dropout', nn.Dropout(self.dropout_p)),
-                        ('linear', nn.Linear(layer_sizes[i], layer_sizes[i + 1])),
-                        ('bnorm', nn.BatchNorm1d(layer_sizes[i + 1]) if (self.use_bn and i < len(layer_sizes) - 2) else nn.Identity()),
-                        ('activation', __allowed_activations__[self.activation] if i < len(layer_sizes) - 2 else nn.Identity())  # up to penultimate layer
-                    ])
-                )
-                for i in range(len(layer_sizes) - 1)]
-            ) for k in self.actions
-        }
-
-        self.transforms = nn.ModuleDict(self.transforms)
-
-    def forward(self, v, action):
-        return self.transforms[action](v)
-
-    def process_batch(self, batch, loss_fn=None):
-        pass  # TODO
